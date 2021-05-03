22ab19126297f23af02942d4b6d8c93d84ae6b9c
David <ber.dev@yandex.ru>
2021-05-03 23:46:21 +0300

    Initial Commit

---
+++ SwiftNetwork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftNetwork
+//
+//  Created by Дэвид Бердников on 03.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
