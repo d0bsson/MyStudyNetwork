b3762d03254ce1a172f2e4e0908517fda46f192e
David <ber.dev@yandex.ru>
2021-05-05 01:42:56 +0300

    create Network class

---
+++ SwiftNetwork/Managers/NetworkManager.swift
@@ -0,0 +1,34 @@
+//
+//  Network.swift
+//  SwiftNetwork
+//
+//  Created by Дэвид Бердников on 04.05.2021.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+    let shared = NetworkManager()
+    
+//    init() {}
+    
+    private func fetchData(from url: String?, with comlition: @escaping ([Friend]) -> Void ) {
+        guard let url = URL(string: URLExamples.UrlJson.rawValue) else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, _, error) in
+            guard let data = data else {
+                print(error?.localizedDescription ?? "No error description" )
+                return
+            }
+            
+            do {
+                let friend = try JSONDecoder().decode([Friend].self, from: data)
+                print(friend)
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        }.resume()
+    }
+}
+
--- SwiftNetwork/ViewControllers/MainViewController.swift
+++ SwiftNetwork/ViewControllers/MainViewController.swift
@@ -9,8 +9,6 @@ import UIKit
 
 class MainViewController: UICollectionViewController {
     
-    
-    
     let userActions = UserActions.allCases
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -29,30 +27,33 @@ class MainViewController: UICollectionViewController {
         let userAction = userActions[indexPath.item]
         
         switch userAction {
-        case .friends: performSegue(withIdentifier: "tableViewSegue", sender: nil)
-        
+        //        case .friends: performSegue(withIdentifier: "tableViewSegue", sender: nil)
+        case .friends: fetchData()
         }
+    }
+    
+    private func fetchData1(from url: String?) {
+        
     }
 }
 
 extension MainViewController {
     private func fetchData() {
         guard let url = URL(string: URLExamples.UrlJson.rawValue) else { return }
-        
+
         URLSession.shared.dataTask(with: url) { (data, _, error) in
             guard let data = data else {
                 print(error?.localizedDescription ?? "No error description" )
                 return
             }
-            
+
             do {
                 let friend = try JSONDecoder().decode([Friend].self, from: data)
                 print(friend)
             } catch let error {
                 print(error.localizedDescription)
             }
-        }.resume()
-        
+        }
     }
 }
 
